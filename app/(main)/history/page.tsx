addf23f6eb6fa3f2b51a006f43071528872c9aea
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-04-30 00:10:39 +0530

    added card on history page

--- app/(main)/history/page.tsx
+++ app/(main)/history/page.tsx
@@ -1,7 +1,13 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { FC } from "react";
-
+import { format, formatRelative, subDays, toDate } from "date-fns";
 interface PageProps {}
 
 const Page: FC<PageProps> = async ({}) => {
@@ -17,9 +23,18 @@ const Page: FC<PageProps> = async ({}) => {
   return (
     <>
       <div>History and activity will be shown here</div>
-      {activity.map((a) => (
-        <div key={a.id}>{a.message}</div>
-      ))}
+      <div className="grid grid-cols-1 mt-10 space-y-3">
+        {activity.map((a) => (
+          <Card key={a.id} className="">
+            <CardHeader>
+              <CardTitle>{a.message}</CardTitle>
+              <CardDescription>
+                {format(new Date(a.createdAt), "yyyy-MM-dd")}
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
     </>
   );
 };
